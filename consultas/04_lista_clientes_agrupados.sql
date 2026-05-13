18aa62026c5cf2684f5682a341c367d0af5b624d
johnalexf <johnalexfr@gmail.com>
2026-05-13 16:02:19 -0500

    feat(consultas): ejercicio 4 ordenar clientes por empleado

---
+++ consultas/04_lista_clientes_agrupados.sql
@@ -0,0 +1,6 @@
+/*
+Obtener la lista de los clientes agrupados por código de empleado 
+asignado, visualizar todas la columnas de la tabla
+*/
+
+SELECT * FROM clientes ORDER BY cod_empleado_cli ASC;
